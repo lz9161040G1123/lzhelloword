d834ec66a7e0d82d001e4dc0f564bef5412b37cf
lz <243651507@qq.com>
2024-10-23 22:02:49 +0800

    test

--- pythonProject/lz_test/application.py
+++ pythonProject/lz_test/application.py
@@ -1,12 +1,16 @@
 import tornado.ioloop
 import tornado.web
 
-from pythonProject.lz_test.handler import MainMnadeler
+from pythonProject.lz_test.handler import MainMnadeler, test1, test2, test3
 
 
 def map_app():
     return tornado.web.Application(
         [
-            (r"/apply", MainMnadeler)
+            (r"/apply", MainMnadeler),
+            (r"/api/login", test1),
+            (r"/api/today-orders", test2),
+            (r"/api/data", test3),
+
         ]
     )
--- pythonProject/lz_test/handler.py
+++ pythonProject/lz_test/handler.py
@@ -3,8 +3,18 @@ import json
 import tornado.web
 import configparser
 import os
+import tornado.ioloop
 from pythonProject.DB_test.DB_operate import dbconf
+
+
 class MainMnadeler(tornado.web.RequestHandler):
+
+    def set_default_headers(self):
+        # 设置CORS头
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+
     @tornado.gen.coroutine
     def post(self):
         argus = self.request.arguments
@@ -18,7 +28,6 @@ class MainMnadeler(tornado.web.RequestHandler):
         res = dbconf(cf, sql1, sec)
         sql2 = "select *from trade"
         res = dbconf(cf, sql2, sec)
-
 
         orderid = self.get_argument("order_id")
         print(orderid)
@@ -48,4 +57,134 @@ class MainMnadeler(tornado.web.RequestHandler):
             'orderid': orderid,
             'data': res
         }
+        self.write(data)
+
+
+class test1(tornado.web.RequestHandler):
+
+    def set_default_headers(self):
+        # 设置CORS头
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+
+    @tornado.gen.coroutine
+    def post(self):
+        data = {
+            'status': 'success'
+        }
+        data = json.dumps(data)
+
+        self.write(data)
+
+    def get(self):
+        data = {
+            'status': 'success'
+        }
+        data = json.dumps(data)
+
+        self.write(data)
+
+
+class test2(tornado.web.RequestHandler):
+
+    def set_default_headers(self):
+        # 设置CORS头
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+
+    @tornado.gen.coroutine
+    def post(self):
+        data1 = {
+            'orderId': '123',
+            'dish': '鱼',
+            'quantity': 2,
+            'danwei': '斤',
+            'price': 200,
+            'totalPrice': 400
+        }
+        data2 = {
+            'orderId': '234',
+            'dish': '肉',
+            'quantity': 3,
+            'danwei': '斤',
+            'price': 200,
+            'totalPrice': 600
+        }
+        data3 = {
+            'orderId': '345',
+            'dish': '白菜',
+            'quantity': 1,
+            'danwei': '斤',
+            'price': 100,
+            'totalPrice': 100
+        }
+        data_list = [data1, data2, data3]
+        data = {
+            'data': data_list
+        }
+
+        data = json.dumps(data, ensure_ascii=False)
+
+        self.write(data)
+
+    def get(self):
+        data1 = {
+            'orderId': '123',
+            'dish': '鱼',
+            'quantity': 2,
+            'danwei': '斤',
+            'price': 200,
+            'totalPrice': 400
+        }
+        data2 = {
+            'orderId': '234',
+            'dish': '肉',
+            'quantity': 3,
+            'danwei': '斤',
+            'price': 200,
+            'totalPrice': 600
+        }
+        data3 = {
+            'orderId': '345',
+            'dish': '白菜',
+            'quantity': 1,
+            'danwei': '斤',
+            'price': 100,
+            'totalPrice': 100
+        }
+        data_list = [data1, data2, data3]
+        data = {
+            'data': data_list
+        }
+
+        data = json.dumps(data, ensure_ascii=False)
+
+        self.write(data)
+
+
+class test3(tornado.web.RequestHandler):
+
+    def set_default_headers(self):
+        # 设置CORS头
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+
+    @tornado.gen.coroutine
+    def post(self):
+        data = {
+            'msg': 'success'
+        }
+        data = json.dumps(data)
+
+        self.write(data)
+
+    def get(self):
+        data = {
+            'msg': 'success'
+        }
+        data = json.dumps(data)
+
         self.write(data)
